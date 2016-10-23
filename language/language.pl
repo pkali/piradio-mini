c179253f37ce48c6cfee8cc81feee5ab14ad27b9
Pecusx <Pecus@poczta.fm>
2016-10-23 22:08:50 +0200

    Add files via upload

---
+++ language/language.pl
@@ -0,0 +1,58 @@
+# Polish text for speech
+consume: Konsumowanie
+rss_display: Kanały RSS
+random: Odtwarzanie losowe
+loading_radio: Wczytuję listę stacji
+alarmminutes: Minuty alarmu
+main_display: Ekran główny
+streaming: Strumieniowanie
+sleep: Uśpienie
+yes: tak
+options_menu: Menu opcji 
+information: Ekran informacji
+select_source: Wybór źródła
+no: nie
+stopping_radio: Zatrzymuję radio
+search_menu: Wyszukiwanie 
+source_media: Biblioteka muzyki
+source_pandora: Radio Pandora
+loading_pandora: Wczytuję listę stacji pandora
+the_time: Godzina
+loading_media: Wczytuję bibliotekę
+repeat: Powtarzanie
+on: włączone 
+search: Szukanie
+off: wyłączone
+alarm: Alarm
+source_radio: Radio internetowe
+timer: Uśpienie
+reload: Odśwież
+voice: Komunikaty
+colour: Kolor
+alarmhours: Godziny alarmu
+Track: Utwór
+Station: Stacja
+h00: dwudziestaczwarta
+h01: pierwsza
+h02: druga
+h03: trzecia
+h04: czwarta
+h05: piąta
+h06: szósta
+h07: siudma
+h08: ósma
+h09: dziewiąta
+h10: dziesiąta
+h11: jedenasta
+h12: dwunasta
+h13: trzynasta
+h14: czternasta
+h15: piętnasta
+h16: szesnasta
+h17: siedemnasta
+h18: osiemnasta
+h19: dziewiętnasta
+h20: dwudziesta
+h21: dwudziestapierwsza
+h22: dwudziestadruga
+h23: dwudziestatrzecia
