5c308109fdebe4e89ef749f8af666d0f5d78c16c
Pecusx <Pecus@poczta.fm>
2017-05-05 23:14:02 +0200

    Install python library to send metadata to icecast server

--- html/scripts/update.sh
+++ html/scripts/update.sh
@@ -6,3 +6,7 @@ modprobe snd-aloop
 
 # Install darkice for encoding said audio stream into mp3
 apt-get -y install darkice
+
+# Install python library to send metadata to icecast server
+ apt-get -y install python-requests
+ 
