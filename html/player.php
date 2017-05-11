8bb73ad92e594e07647a6300b6edd1fb30d57584
Pecusx <Pecus@poczta.fm>
2017-05-11 18:02:24 +0200

    Web player problem in new version of Opera browser

--- html/player.php
+++ html/player.php
@@ -3,8 +3,8 @@ $line1 = file_get_contents( "/tmp/radiod/line1.txt" );
 $czyplayer = strpos($line1, "*");
 if ($czyplayer != false) {
 	list($realHost,)=explode(':',$_SERVER['HTTP_HOST']);
-	echo '<audio controls><source src="http://';
+	echo '<audio src="http://';
 	echo $realHost;
-	echo ':8001/mpd" type="audio/mp3">Your browser does not support the audio element.</audio>';
+	echo ':8001/mpd" type="audio/mp3" controls="true">Your browser does not support the audio element.</audio>';
 }
 ?>
