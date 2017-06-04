c64206c8e09a3268df5401ad810f366404f0bb12
Pecusx <Pecus@poczta.fm>
2017-06-04 19:32:12 +0200

    For future installs

--- html/scripts/update.sh
+++ html/scripts/update.sh
@@ -1,8 +1,8 @@
 # Set up new stream encoder config files
-cp /usr/share/radio/hardware/audio/darkice.cfg /etc/darkice.cfg
+#cp /usr/share/radio/hardware/audio/darkice.cfg /etc/darkice.cfg
 
 # install MPD 0.19.12 from package
-cp /etc/mpd.conf /usr/share/radio/mpd.conf
-dpkg --force-all -i /usr/share/radio/software/mpd_0.19.12-1~bpo8+1_armhf.deb
-cp /usr/share/radio/mpd.conf /etc/mpd.conf
-rm /usr/share/radio/mpd.conf
+#cp /etc/mpd.conf /usr/share/radio/mpd.conf
+#dpkg --force-all -i /usr/share/radio/software/mpd_0.19.12-1~bpo8+1_armhf.deb
+#cp /usr/share/radio/mpd.conf /etc/mpd.conf
+#rm /usr/share/radio/mpd.conf
