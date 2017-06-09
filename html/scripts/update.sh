ce63f75767d3c548d888412d359345362571338d
Pecusx <Pecus@poczta.fm>
2017-06-09 19:03:57 +0200

    bad filename

--- html/scripts/update.sh
+++ html/scripts/update.sh
@@ -3,7 +3,7 @@
 
 # install MPD 0.19.12 from package
 #cp /etc/mpd.conf /usr/share/radio/mpd.conf
-#dpkg --force-all -i /usr/share/radio/software/mpd_0.19.12-1-bpo8+1_armel.deb
+#dpkg --force-all -i /usr/share/radio/software/mpd_0.19.12-1~bpo8+1_armhf.deb
 #cp /usr/share/radio/mpd.conf /etc/mpd.conf
 #rm /usr/share/radio/mpd.conf
 
