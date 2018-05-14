c9afd94c3373f7856d87bf86105ce63ed4cf0765
Pecusx <Pecus@poczta.fm>
2018-05-14 20:26:51 +0200

    Prepare to scroll in web interface (mobile version).

--- html/lcd.php
+++ html/lcd.php
@@ -1,8 +1,16 @@
 <?php
-$line1 = file_get_contents( "/tmp/radiod/line1.txt" );
-$line2 = file_get_contents( "/tmp/radiod/line2.txt" );
-$line3 = file_get_contents( "/tmp/radiod/line3.txt" );
-$line4 = file_get_contents( "/tmp/radiod/line4.txt" );
+$msg = $_GET['scroll'];
+if ($msg=="True") {
+	$line1 = file_get_contents( "/tmp/radiod/line1s.txt" );
+	$line2 = file_get_contents( "/tmp/radiod/line2s.txt" );
+	$line3 = file_get_contents( "/tmp/radiod/line3s.txt" );
+	$line4 = file_get_contents( "/tmp/radiod/line4s.txt" );
+} else {
+	$line1 = file_get_contents( "/tmp/radiod/line1.txt" );
+	$line2 = file_get_contents( "/tmp/radiod/line2.txt" );
+	$line3 = file_get_contents( "/tmp/radiod/line3.txt" );
+	$line4 = file_get_contents( "/tmp/radiod/line4.txt" );
+}
 $line1 = str_replace(" ","&nbsp;",$line1);
 $line2 = str_replace(" ","&nbsp;",$line2);
 $line3 = str_replace(" ","&nbsp;",$line3);
